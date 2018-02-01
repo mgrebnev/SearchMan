4c4ebe4f0fabeed56e5513743e1eccc101540908
Максим Гребнев <solightingstats@yandex.ru>
2018-02-02 00:50:02 +0300

    Add pretty font

--- src/main/java/ru/searchman/Main.java
+++ src/main/java/ru/searchman/Main.java
@@ -5,14 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("SettingsForm.fxml"));
+
         primaryStage.setTitle("SearchMan v1.0.1");
 
         Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
@@ -20,7 +21,10 @@ public class Main extends Application {
         primaryStage.setY((primScreenBounds.getHeight() - 332) / 2);
         primaryStage.setResizable(false);
 
-        primaryStage.setScene(new Scene(root, primaryStage.getWidth(), primaryStage.getHeight()));
+        Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight());
+        scene.getStylesheets().add(Main.class.getResource("/css/application-style.css").toExternalForm());
+
+        primaryStage.setScene(scene);
         primaryStage.setOnCloseRequest(event -> System.exit(0));
         primaryStage.show();
     }
